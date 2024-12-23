9bf1e7779e46bb8df7f63f5a06402740f665eb7b
Djimbo <djimbosfan@mail.ru>
2024-12-23 17:52:03 +0300

    Update dj_install.sh

--- dj_install.sh
+++ dj_install.sh
@@ -1,18 +1,18 @@
-sudo apt-get update &&
-sudo apt-get upgrade -y &&
+sudo apt update &&
+sudo apt upgrade -y &&
 
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl software-properties-common -y &&
 sudo apt install software-properties-common &&
 sudo add-apt-repository ppa:deadsnakes/ppa -y &&
 
-sudo apt-get install supervisor -y &&
-sudo apt-get install screen -y &&
-sudo apt-get install p7zip-full -y &&
-sudo apt-get install unzip -y &&
-sudo apt-get install python3.10 -y &&
-sudo apt-get install python3-pip -y &&
-sudo apt-get install python3.10-dev -y &&
-sudo apt-get install python3.10-distutils -y &&
+sudo apt install supervisor -y &&
+sudo apt install screen -y &&
+sudo apt install p7zip-full -y &&
+sudo apt install unzip -y &&
+sudo apt install python3.10 -y &&
+sudo apt install python3-pip -y &&
+sudo apt install python3.10-dev -y &&
+sudo apt install python3.10-distutils -y &&
 
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1 &&
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2 &&
